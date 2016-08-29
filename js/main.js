c5f735f4003aa372bf0f3e60abda120febb8f69e
amitkishore007 <kishoreamit5@gmail.com>
2016-08-29 11:21:40 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -210,8 +210,8 @@ jQuery(document).ready(function($) {
     /* ---------------------------------------------------------------------- */
     $(".map-location").on('click', function() {
         //set your google maps parameters
-        var latitude = 37.775,
-            longitude = -122.4183333,
+        var latitude = 28.4135012,
+            longitude = 77.0441276,
             map_zoom = 14;
 
         //google map custom marker icon - .png fallback for IE11
